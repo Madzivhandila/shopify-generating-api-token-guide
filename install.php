53f60ad45da313aca2e97114209762e3c594468b
Madzivhandila <75025282+Madzivhandila@users.noreply.github.com>
2020-11-25 15:58:01 +0200

    Update install.php

--- install.php
+++ install.php
@@ -2,9 +2,9 @@
 
 // Set variables for our request
 $shop = $_GET['shop'];
-$api_key = "1r30mrvCFMfq2DLGuIXyY2veEJVgTtDD";
+$api_key = "6def24808aa1391874c3230a349b173b";
 $scopes = "read_orders,write_products";
-$redirect_uri = "http://localhost/generate_token.php";
+$redirect_uri = "http://Skelem.com/generate_token.php";
 
 // Build install/approval URL to redirect to
 $install_url = "https://" . $shop . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
